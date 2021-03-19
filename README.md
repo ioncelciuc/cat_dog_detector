69a141ff7c147f2f927f78fd83434f0f94d64287
ioncelciuc <ioncelciuc2001@gmail.com>
2021-03-19 20:42:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cat_dog_detector
+ An app that detects if an image contains a cat or a dog
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:cat_dog_detector/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
