ae4a2ec5d892f8926904392d4d5d99a8c75900e8
ioncelciuc <ioncelciuc2001@gmail.com>
2021-03-20 15:16:21 +0200

    Finished app

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,6 +12,64 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   bool _loading = true;
+  File _image;
+  List _output;
+  final picker = ImagePicker();
+
+  detectImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 2,
+      threshold: 0.6,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _output = output;
+      _loading = false;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+      model: 'assets/model_unquant.tflite',
+      labels: 'assets/labels.txt',
+    );
+  }
+
+  pickCameraImage() async {
+    var image = await picker.getImage(source: ImageSource.camera);
+    if (image == null) return null;
+    setState(() {
+      _image = File(image.path);
+    });
+
+    detectImage(_image);
+  }
+
+  pickGalleryImage() async {
+    var image = await picker.getImage(source: ImageSource.gallery);
+    if (image == null) return null;
+    setState(() {
+      _image = File(image.path);
+    });
+
+    detectImage(_image);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((value) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,14 +86,35 @@ class _HomeState extends State<Home> {
               Container(
                 child: _loading
                     ? Image.asset('assets/cat_dog_icon.png')
-                    : Container(),
+                    : Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 250,
+                              child: Image.file(_image),
+                            ),
+                            SizedBox(height: 20),
+                            _output != null
+                                ? Text(
+                                    '${_output[0]['label']}',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  )
+                                : Container(),
+                          ],
+                        ),
+                      ),
               ),
               SizedBox(height: 50),
               Row(
                 children: [
                   Expanded(
                     child: ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        pickCameraImage();
+                      },
                       child: Padding(
                         padding: EdgeInsets.symmetric(vertical: 16),
                         child: Text(
@@ -44,7 +127,9 @@ class _HomeState extends State<Home> {
                   SizedBox(width: 32),
                   Expanded(
                     child: ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        pickGalleryImage();
+                      },
                       child: Padding(
                         padding: EdgeInsets.symmetric(vertical: 16),
                         child: Text(
